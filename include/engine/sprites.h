90fc5f522ad07b114fdbb2a5c2c89a7a67073014
IoriBranford <ioribranford@gmail.com>
2024-12-16 13:22:18 -0800

    Expose key sprites functions.

--- include/engine/sprites.h
+++ include/engine/sprites.h
@@ -1,6 +1,21 @@
 #ifndef E222BE36_5310_48C7_9586_70543D02A1F0
 #define E222BE36_5310_48C7_9586_70543D02A1F0
 
+#include <raylib-tmx.h>
+#include <raylib-aseprite.h>
+
 typedef struct Sprite Sprite;
+
+void InitSprites(unsigned n);
+void CloseSprites();
+
+void PruneSprites();
+void UpdateSprites();
+void DrawSprites();
+
+size_t NumSpritesAvailable();
+Sprite* NewRectangleSprite(Rectangle rect, Vector2 origin, float rotationDeg, Color color);
+
+void ReleaseSprite(Sprite* sprite);
 
 #endif /* E222BE36_5310_48C7_9586_70543D02A1F0 */
