25cb05126d39dae82c36509ff50e4fc0b44d7f3d
IoriBranford <ioribranford@gmail.com>
2024-12-18 15:34:31 -0800

    Simple lighting effect on confetti.

--- src/game/confetti.c
+++ src/game/confetti.c
@@ -5,11 +5,21 @@
 void Task_Confetti(void *p) {
     Sprite *spr = p;
 
-    spr->animTimer += GetFrameTime();
+    spr->animTimer += 1;
 
-    spr->size.x = (cosf(spr->animTimer * PI * 8) + 1) * 4;
+    float sinAngleRad = sinf(spr->animTimer * PI * 4 / 60);
+    spr->size.x = (sinAngleRad + 1) * 8 / 2;
     spr->origin.x = spr->size.x/2;
-    spr->position.y += 8;
+    spr->position.y += 2;
+
+    Color color = spr->color;
+    unsigned sat = (sinAngleRad + 1) * 256 / 2;
+    if (sat > 255) sat = 255;
+    else if (sat < 1) sat = 1;
+    if (color.r) color.r = sat;
+    if (color.g) color.g = sat;
+    if (color.b) color.b = sat;
+    spr->color = color;
 
     if (spr->position.y >= GetScreenHeight() + spr->size.y) {
         Task *t = spr->exdata;
@@ -23,8 +33,8 @@ void AddConfetti() {
         return;
 
     Rectangle rect;
-    rect.width = 8.f ; //* GetRandomValue(1,4);
-    rect.height = 8.f; // * GetRandomValue(1,4);
+    rect.width = 0;
+    rect.height = 8;
     
     Vector2 origin = {rect.width/2, rect.height/2};
     rect.x = GetRandomValue(origin.x, GetScreenWidth()-origin.x);
@@ -32,14 +42,10 @@ void AddConfetti() {
 
     float rotation = GetRandomValue(0, 89);
 
-    Color color = {
-        GetRandomValue(0,255),
-        GetRandomValue(0,255),
-        GetRandomValue(0,255),
-        255
-    };
+    Color color = ColorFromHSV(GetRandomValue(0,5) * 60, 1, 1);
 
     Sprite *sprite = NewRectangleSprite(rect, origin, rotation, color);
     Task *task = NewTask(Task_Confetti, sprite, 2);
     sprite->exdata = task;
+    sprite->animTimer = 0;
 }
