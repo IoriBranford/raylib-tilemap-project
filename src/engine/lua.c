cf5e34122c4e71bfe40b8d2540c6ca23f4957f01
IoriBranford <ioribranford@gmail.com>
2025-01-03 19:07:59 -0800

    Simplify Lua execution model. Run Lua script as a yieldable program.

--- src/engine/lua.c
+++ src/engine/lua.c
@@ -7,148 +7,53 @@
 
 static lua_State *lua;
 
-void EndLuaTask(Task *t) {
-    intptr_t ref = t->data;
-    luaL_unref(lua, LUA_REGISTRYINDEX, ref);
-    EndTask(t);
-}
-
-int Lua_EndTask(lua_State *l) {
-    assert(lua_istable(l, -1)); // lua: self
-    lua_pushstring(l, "__task"); // lua: self, "__task"
-    lua_gettable(l, -2); // lua: self, task
-    assert(lua_islightuserdata(l, -1));
-    Task *t = lua_touserdata(l, -1);
-    lua_pop(l, 2);
-    assert(t);
-    EndLuaTask(t);
-    return 0;
-}
-
-void Task_LuaFunction(Task *t) {
-    intptr_t ref = t->data;
+void Task_ResumeLuaThread(Task *t) {
+    int ref = t->data;
     lua_rawgeti(lua, LUA_REGISTRYINDEX, ref);
-    assert(lua_istable(lua, -1)); // lua: self
-
-    lua_pushstring(lua, "__call"); // lua: self, "__call"
-    lua_gettable(lua, -2);
-    assert(lua_isfunction(lua, -1)); // lua: self, function
-
-    lua_pushvalue(lua, -2); // lua: self, function, self
-    int result = lua_pcall(lua, 1, 0, 0); // lua: self
-    if (result != LUA_OK) { // lua: self, error
-        printf(luaL_tolstring(lua, -1, NULL));
-        printf("\n");
-        lua_pop(lua, 1); // lua: self
-        EndLuaTask(t);
-    }
-    lua_pop(lua, 1); // lua:
-}
-
-void Task_LuaClass(Task *t) {
-    intptr_t ref = t->data;
-    lua_rawgeti(lua, LUA_REGISTRYINDEX, ref);
-    assert(lua_istable(lua, -1)); // lua: self
-
-    lua_pushstring(lua, "update"); // lua: self, "update"
-    lua_gettable(lua, -2);
-    if (lua_isfunction(lua, -1)) { // lua: self, update
-        lua_pushvalue(lua, -2); // lua: self, update, self
-        int result = lua_pcall(lua, 1, 0, 0); // lua: self
-        if (result != LUA_OK) { // lua: self, error
-            printf(luaL_tolstring(lua, -1, NULL));
-            printf("\n");
-            lua_pop(lua, 1); // lua: self
-            EndLuaTask(t);
-        }
-    }
-    lua_pop(lua, 1); // lua:
-}
-
-Task* NewLuaClassTask(int priority) {
-    lua_newtable(lua); // lua: class, self
-    lua_pushvalue(lua, -2); // lua: class, self, class
-    lua_setmetatable(lua, -2); // lua: class, self
-
-    intptr_t ref = luaL_ref(lua, LUA_REGISTRYINDEX); // lua: class
-
-    Task *task = NewTask(Task_LuaClass, (void*)ref, priority);
-    lua_rawgeti(lua, LUA_REGISTRYINDEX, ref); // lua: class, self
-    lua_pushstring(lua, "__task");// lua: class, self, "__task"
-    lua_pushlightuserdata(lua, task); // lua: class, self, "__task", task
-    lua_settable(lua, -3);  // lua: class, self
-    lua_pop(lua, 1); // lua: class
-    
-    lua_pushstring(lua, "start");// lua: class, "start"
-    lua_gettable(lua, -2);  // lua: class, start
-    if (lua_isfunction(lua, -1)) {
-        lua_rawgeti(lua, LUA_REGISTRYINDEX, ref); // lua: class, start, self
-        int result = lua_pcall(lua, 1, 0, 0); // lua: class
-        if (result != LUA_OK) { // lua: class, error
-            printf(luaL_tolstring(lua, -1, NULL));
-            printf("\n");
-            lua_pop(lua, 1); // lua: class
-        }
+    lua_State *thread = lua_tothread(lua, -1);
+    int nResults = 0;
+    int result = lua_resume(thread, NULL, 0, &nResults);
+    if (result == LUA_OK) {
+        luaL_unref(lua, LUA_REGISTRYINDEX, ref);
+        EndTask(t);
+    } else if (result == LUA_YIELD) {
+        t->priority = luaL_optinteger(thread, 1, 0);
     } else {
-        lua_pop(lua, 1); // lua: class
+        fprintf(stderr, "LUA: %s\n", lua_tostring(lua, -1));
     }
-    lua_pop(lua, 1); // lua:
-    return task;
+    lua_pop(thread, lua_gettop(thread));
+    lua_pop(lua, lua_gettop(lua));
 }
 
-Task* NewLuaFunctionTask(int priority) { // lua: function
-    lua_newtable(lua); // lua: function, self
-
-    lua_pushstring(lua, "__call");// lua: function, self, "__call"
-    lua_pushvalue(lua, -3); // lua: function, self, "__call", function
-    lua_settable(lua, -3);  // lua: function, self
-
-    intptr_t ref = luaL_ref(lua, LUA_REGISTRYINDEX); // lua: function
-    lua_pop(lua, 1); // lua:
-
-    Task *task = NewTask(Task_LuaFunction, (void*)ref, priority);
-    lua_rawgeti(lua, LUA_REGISTRYINDEX, ref); // lua: self
-    lua_pushstring(lua, "__task");// lua: self, "__task"
-    lua_pushlightuserdata(lua, task); // lua: self, "__task", task
-    lua_settable(lua, -3);  // lua: self
-    lua_pop(lua, 1); // lua:
-    return task;
-}
-
-int RequireLuaModule(lua_State *l) {
-    luaL_checkstring(l, 1); // lua: modulePath
-    lua_getglobal(l, "require"); // lua: modulePath, require
-    lua_pushvalue(l, -2); // lua: modulePath, require, modulePath
-    lua_call(l, 1, 1); // lua: modulePath, ?
-
-    if (lua_istable(l, -1)) { // lua: modulePath, class
-        lua_pushstring(l, "__index"); // lua: modulePath, class, "__index"
-        lua_pushvalue(l, -2); // lua: modulePath, class, "__index", class
-        lua_settable(l, -3); // lua: modulePath, class
-
-        lua_pushstring(l, "update"); // lua: modulePath, class, "update"
-        lua_gettable(l, -2); // lua: modulePath, class, update
-        if (!lua_isfunction(l, -1)) {
-            printf("Warning: no update function in %s\n", lua_tostring(l, 1));
+int RunLua(const char *luaFile) {
+    if (lua_getglobal(lua, luaFile) != LUA_TFUNCTION) {
+        lua_pop(lua, 1);
+        int error = luaL_loadfile(lua, luaFile);
+        if (error) {
+            fprintf(stderr, "LUA: %s\n", lua_tostring(lua, -1));
+            lua_pop(lua, 1);
+            return error;
         }
-        lua_pop(l, 1); // lua: modulePath, class
+        lua_pushvalue(lua, -1);
+        lua_setglobal(lua, luaFile);
     }
 
-    return 1;
-}
-
-Task* NewLuaTask(const char *luaModule, int priority) {
-    luaL_requiref(lua, luaModule, RequireLuaModule, 0);
-
-    if (lua_isfunction(lua, -1)) { // lua: function
-        return NewLuaFunctionTask(priority);
+    lua_State *thread = lua_newthread(lua);
+    lua_pushvalue(lua, 1);
+    lua_xmove(lua, thread, 1);
+    int nResults = 0;
+    int result = lua_resume(thread, NULL, 0, &nResults);
+    if (result == LUA_OK) {
+    } else if (result == LUA_YIELD) {
+        int priority = luaL_optinteger(thread, 1, 0);
+        int ref = luaL_ref(lua, LUA_REGISTRYINDEX);
+        NewTask(Task_ResumeLuaThread, ref, priority);
+    } else {
+        fprintf(stderr, "LUA: %s\n", lua_tostring(thread, -1));
     }
-    if (lua_istable(lua, -1)) { // lua: class
-        return NewLuaClassTask(priority);
-    }
-
-    printf("Can't start a task from a %s\n", lua_typename(lua, lua_type(lua, -1)));
-    return NULL;
+    lua_pop(thread, lua_gettop(thread));
+    lua_pop(lua, lua_gettop(lua));
+    return result;
 }
 
 void CloseLua() {
