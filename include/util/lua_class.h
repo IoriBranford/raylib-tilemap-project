32573742b644e77646c83b74f9cdc3b1e3c05efc
IoriBranford <ioribranford@gmail.com>
2025-01-05 00:02:23 -0800

    Macros for managing getters and setters in C struct metatables.

---
+++ include/util/lua_class.h
@@ -0,0 +1,38 @@
+#ifndef CD425245_A2F7_4AB2_ADAB_E5F9A1924686
+#define CD425245_A2F7_4AB2_ADAB_E5F9A1924686
+
+#define class_getter(cls, fieldtype, field) \
+int L_##cls##_get_##field(lua_State *l) { \
+    cls **o = luaL_checkudata(l, 1, #cls); \
+    lua_push##fieldtype(l, (*o)->field); \
+    return 1; \
+}
+
+#define class_setter(cls, fieldtype, field) \
+int L_##cls##_set_##field(lua_State *l) { \
+    cls **o = luaL_checkudata(l, 1, #cls); \
+    (*o)->field = lua_to##fieldtype(l, 2); \
+    return 0; \
+}
+
+#define class_getter_and_setter(cls, fieldtype, field) \
+    class_getter(cls, fieldtype, field) \
+    class_setter(cls, fieldtype, field)
+
+#define class_init_getter(cls, field) { \
+    lua_pushstring(l, "get_" #field); \
+    lua_pushcfunction(l, L_##cls##_get_##field); \
+    lua_settable(l, -3); \
+}
+
+#define class_init_setter(cls, field) { \
+    lua_pushstring(l, "set_" #field); \
+    lua_pushcfunction(l, L_##cls##_set_##field); \
+    lua_settable(l, -3); \
+}
+
+#define class_init_getter_and_setter(cls, field) \
+    class_init_getter(cls, field) \
+    class_init_setter(cls, field)
+
+#endif /* CD425245_A2F7_4AB2_ADAB_E5F9A1924686 */
