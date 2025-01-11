8086bb4bd356b0c8f9f64fe1248530cc1bd8f7d8
IoriBranford <ioribranford@gmail.com>
2025-01-10 17:09:32 -0800

    Update prospective confetti script.

--- resources/confetti.lua
+++ resources/confetti.lua
@@ -1,44 +1,44 @@
+local x, y = ...
+x = x or 0
+y = y or 0
+
 local w, h = 0, 8
 local ox, oy = w/2, h/2
-local x = math.random(ox, GetScreenWidth()-ox)
-local y = 0
 local rotationDeg = math.random() * math.pi / 2
 local hueDeg = math.random(0, 5) * 60
-local color = ColorFromHSV(hueDeg, 1, 1)
+local red, green, blue = color.fromHSV(hueDeg, 1, 1)
 
-local body = Body(x, y, rotationDeg);
-body:SetVelocity(0, 4)
-body:SetAngularVelocity(math.pi/16)
-body:AddCircle(0, 0, ox)
+local body = physics.body();
+body:setPosition(x, y)
+body:setVelocity(0, 4)
+body:setAngularVelocity(math.pi/16)
+physics.circleshape(oy)
 
-local sprite = RectangleSprite(x, y, w, h, ox, oy, rotationDeg, color)
-
-while true do
+local spr = sprite.rectangle(x, y, w, h, ox, oy, rotationDeg, red, green, blue)
+local timer = 0
+while spr.nearcamera do
     coroutine.yield()
 
-    body:UpdateSprite(sprite)
-    if not sprite:IsOnScreen() then
-        break
-    end
+    body:updateSprite(spr)
 
-    local mass = body.mass
-    body:SetForce(0, -mass/64)
+    local mass = body.Mass
+    body:setForce(0, -mass/64)
 
-    local spriteAnimTimer = sprite:AdvanceAnimTimer()
+    timer = timer + raylib.FrameTime
 
-    local sin = math.sin(spriteAnimTimer * math.pi / 4)
+    local sin = math.sin(timer * math.pi / 4)
     local w = (sin + 1) * 4
-    sprite.width = w
-    sprite.originX = w/2
+    spr.width = w
+    spr.originX = w/2
 
-    local r, g, b, a = sprite:GetColor()
+    local r, g, b, a = spr:getcolor()
     local sat = (sin + 1) * 128
     if sat > 255 then sat = 255 elseif sat < 1 then sat = 1 end
     if r ~= 0 then r = sat end
     if g ~= 0 then g = sat end
     if b ~= 0 then b = sat end
-    sprite:SetColor(r, g, b, a)
+    spr:setcolor(r, g, b, a)
 end
 
-body:Destroy()
-sprite:Release()
+body:RemoveFromSpace()
+spr.alpha = 0
