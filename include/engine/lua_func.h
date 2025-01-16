ee4f098489831c910660c75eedc3622a68ee12f9
IoriBranford <ioribranford@gmail.com>
2025-01-15 23:57:07 -0800

    l_func_1_ud

--- include/engine/lua_func.h
+++ include/engine/lua_func.h
@@ -5,6 +5,16 @@
 #define l_func_1_0(f, at) int L_##f(lua_State *l) { f(lua_check##at(l, 1)); return 0; }
 #define l_func_0_1(f, rt) int L_##f(lua_State *l) { lua_push##rt(l, f()); return 1; }
 #define l_func_1_1(f, at, rt) int L_##f(lua_State *l) { lua_push##rt(l, f(lua_check##at(l, 1))); return 1; }
+#define l_func_1_ud(f, at, ud) int L_##f(lua_State *l) { \
+    ud *o = f(lua_to##at(l, 1)); \
+    if (o) { \
+        ud** od = lua_newuserdata(l, sizeof(ud*)); \
+        *od = o; \
+        luaL_setmetatable(l, #ud); \
+        return 1; \
+    } \
+    return 0; \
+}
 
 #define l_func_reg(f) {.name = #f, .func = L_##f}
 
