a4e1d673a7a7a4454a9456b16e169854f56f4b4d
IoriBranford <ioribranford@gmail.com>
2025-01-16 10:35:22 -0800

    Try spawning confetti on key press.

--- lua/confettiscreen.lua
+++ lua/confettiscreen.lua
@@ -2,15 +2,11 @@ return function()
     local map = LoadMap("resources/desert.tmx")
     local t = 0
     local obj = map:find_object_by_id(16)
-    local FRAMES_PER_CONFETTI = 2
+    repeat until GetKeyPressed() == 0
     while true do
-        if t == 0 then
+        coroutine.yield()
+        while GetKeyPressed() ~= 0 do
             task.run("confettiobject", 1, obj, map)
         end
-        t = t + 1
-        if t >= FRAMES_PER_CONFETTI then
-            t = 0
-        end
-        coroutine.yield()
     end
 end
