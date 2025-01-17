1036742cadc45d43ceb73aa5d524c900f113f4d4
IoriBranford <ioribranford@gmail.com>
2025-01-17 14:41:39 -0800

    Set up engine main loop for web.

--- src/engine/engine.c
+++ src/engine/engine.c
@@ -2,6 +2,10 @@
 
 #include "raylib.h"
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
 static void Nop() {}
 
 static Phase currentPhase = {
@@ -20,27 +24,32 @@ void InitEngine() {
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
     InitAudioDevice();
     // TODO: Initialize all required variables and load all required data here!
-
-    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
     //--------------------------------------------------------------------------------------
 }
 
+void UpdateEngine() {
+    // Update
+    //----------------------------------------------------------------------------------
+    currentPhase.update();
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    currentPhase.draw();
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
+
 void RunEngine() {
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateEngine, 0, 1);
+#else
+    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        currentPhase.update();
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        currentPhase.draw();
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+        UpdateEngine();
+#endif
 }
 
 void StopEngine() {
