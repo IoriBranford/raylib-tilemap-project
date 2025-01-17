73bb54dfb3fd42123e5b59bd6cf105d8c9f3d027
IoriBranford <ioribranford@gmail.com>
2025-01-16 17:06:00 -0800

    Try playing a sound in Lua.

--- lua/confettiscreen.lua
+++ lua/confettiscreen.lua
@@ -1,11 +1,13 @@
 return function()
     local map = LoadMap("resources/desert.tmx")
+    local sound = LoadSound("resources/決定ボタンを押す7.mp3")
     local t = 0
     local obj = map:find_object_by_id(16)
     repeat until GetKeyPressed() == 0
     while true do
         coroutine.yield()
         while GetKeyPressed() ~= 0 do
+            sound:Play()
             task.run("confettiobject", 1, obj, map)
         end
     end
