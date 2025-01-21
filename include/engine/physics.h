4325666e6581a3e48ff746bdbcfcd4f77d15ebc8
IoriBranford <ioribranford@gmail.com>
2025-01-20 17:08:04 -0800

    GiveBodyTMXLayerShapes

--- include/engine/physics.h
+++ include/engine/physics.h
@@ -11,6 +11,7 @@ void UpdatePhysics();
 cpBody* NewBody(cpFloat x, cpFloat y, cpFloat rotationRad);
 cpBody* NewTMXObjectBody(tmx_object *obj, tmx_map *map);
 cpBody* GiveBodyTMXObjectShapes(cpBody *body, tmx_object *obj, tmx_tile **maptiles, cpVect offset);
+cpBody* GiveBodyTMXLayerShapes(cpBody *body, tmx_layer *layer, tmx_map *map, cpVect offset);
 void ReleaseBody(cpBody *body);
 void ReleaseOrphanedShape(cpShape *shape);
 
