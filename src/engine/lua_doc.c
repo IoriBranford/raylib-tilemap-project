f41f2596a08b71b1461b1aff70930d744225d9bd
IoriBranford <ioribranford@gmail.com>
2025-01-21 14:36:17 -0800

    Start writing doc in LuaCATS format.

--- src/engine/lua_doc.c
+++ src/engine/lua_doc.c
@@ -113,6 +113,55 @@ int L_doc_load(lua_State *l) {
     return 1;
 }
 
+void L_doc_funcs_write(lua_State *l, FILE *file, const char *cls) {
+    int nFuncs = lua_objlen(l, -1);
+    for (int i = 1; i <= nFuncs; ++i) {
+        lua_rawgeti(l, -1, i);
+
+        lua_getfield(l, -1, "description");
+        const char *s = lua_tostring(l, -1);
+        fprintf(file, "--- %s\n", s);
+        lua_pop(l, 1);
+
+        lua_getfield(l, -1, "name");
+        s = lua_tostring(l, -1);
+        fprintf(file, "function %s%s%s(",
+            cls ? cls : "",
+            cls ? ":" : "",
+            s);
+        lua_pop(l, 1);
+
+        // TODO args
+
+        fprintf(file, ") end\n\n");
+        lua_pop(l, 1);
+    }
+}
+
+void L_doc_types_write(lua_State *l, FILE *file) {
+    int nTypes = lua_objlen(l, -1);
+    for (int i = 1; i <= nTypes; ++i) {
+        lua_rawgeti(l, -1, i);
+        luaL_checktype(l, -1, LUA_TTABLE);
+
+        lua_getfield(l, -1, "description");
+        fprintf(file, "--- %s\n", lua_tostring(l, -1));
+        lua_pop(l, 1);
+
+        lua_getfield(l, -1, "name");
+        const char *name = lua_tostring(l, -1);
+        fprintf(file, "---@class %s\n", name);
+        fprintf(file, "local %s = {}\n\n", name);
+        lua_pop(l, 1);
+
+        lua_getfield(l, -1, "functions");
+        L_doc_funcs_write(l, file, name);
+        lua_pop(l, 1);
+
+        lua_pop(l, 1);
+    }
+}
+
 int L_doc_save(lua_State *l) {
     lua_getglobal(l, "require");
     lua_pushstring(l, "pl.pretty");
@@ -124,9 +173,21 @@ int L_doc_save(lua_State *l) {
     if (lua_pcall(l, 1, 1, 0) != LUA_OK) lua_error(l);
     lua_concat(l, 2);
     
-    FILE *docFile = fopen("lua/api.lua", "w");
-    fprintf(docFile, "%s", lua_tostring(l, -1));
-    fclose(docFile);
+    FILE *file = fopen("lua/api.lua", "w");
+    fprintf(file, "%s", lua_tostring(l, -1));
+    fclose(file);
+    lua_pop(l, 1);
+
+    file = fopen("lua/doc.lua", "w");
+    fprintf(file, "---@meta\n\n");
+    L_doc(l);
+    L_doc_funcs(l);
+    L_doc_funcs_write(l, file, NULL);
+    lua_pop(l, 1);
+    L_doc_types(l);
+    L_doc_types_write(l, file);
+    lua_pop(l, 1);
+    fclose(file);
 
     return 0;
 }
