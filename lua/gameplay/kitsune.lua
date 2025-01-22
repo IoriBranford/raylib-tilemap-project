5817ea484beb6619af5986d71defc5315852ecf1
IoriBranford <ioribranford@gmail.com>
2025-01-21 18:26:07 -0800

    Try input actions in Lua.

--- lua/gameplay/kitsune.lua
+++ lua/gameplay/kitsune.lua
@@ -1,15 +1,26 @@
 return function(obj, map)
+    MapInputsToActions({
+        KEY_LEFT = "movex-",
+        KEY_RIGHT = "movex+",
+    })
     local sprite = obj:new_sprite(map)
+    local tilename = "tile_stand0"
+    sprite.tilenamedifnew = tilename
     while true do
         coroutine.yield()
-        if IsKeyDown(KEY_LEFT) then
-            sprite.tilenamedifnew = "tile_walk2"
-        elseif IsKeyDown(KEY_RIGHT) then
-            sprite.tilenamedifnew = "tile_walk0"
-        elseif IsKeyReleased(KEY_LEFT) then
-            sprite.tilenamedifnew = "tile_stand2"
-        elseif IsKeyReleased(KEY_RIGHT) then
-            sprite.tilenamedifnew = "tile_stand0"
+        local goingLeft, goingRight = IsActionDown("movex-"), IsActionDown("movex+")
+        if goingLeft and not goingRight then
+            tilename = "tile_walk2"
+            sprite.tilenamedifnew = tilename
+        elseif not goingLeft and goingRight then
+            tilename = "tile_walk0"
+            sprite.tilenamedifnew = tilename
+        elseif tilename == "tile_walk0" then
+            tilename = "tile_stand0"
+            sprite.tilenamedifnew = tilename
+        elseif tilename == "tile_walk2" then
+            tilename = "tile_stand2"
+            sprite.tilenamedifnew = tilename
         end
     end
 end
