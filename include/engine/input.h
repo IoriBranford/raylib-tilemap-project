eb17d3a73e5e401d41c998010077277683d957d3
IoriBranford <ioribranford@gmail.com>
2025-01-21 16:32:33 -0800

    MapInputsToActions_ht, IsActionPressed, IsActionDown, IsActionReleased, GetActionPosition. Privatize ActionState members.

--- include/engine/input.h
+++ include/engine/input.h
@@ -1,16 +1,16 @@
 #ifndef BBC0DE3D_44BF_4F09_9B2A_797A6321B577
 #define BBC0DE3D_44BF_4F09_9B2A_797A6321B577
 
-typedef struct {
-    float position, lastPosition;
-    int pressed, down, released;
-} ActionState;
-
 void InitInput();
 void CloseInput();
 void UpdateInput();
 
 void MapInputToAction(const char *input, const char *action);
-ActionState* GetActionState(const char *action);
+void MapInputsToActions_ht(void *newInputActions);
+
+int IsActionPressed(const char *action);
+int IsActionDown(const char *action);
+int IsActionReleased(const char *action);
+float GetActionPosition(const char *action);
 
 #endif /* BBC0DE3D_44BF_4F09_9B2A_797A6321B577 */
