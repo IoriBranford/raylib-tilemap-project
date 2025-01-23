ad7d53995926a0ded3fb4b3dbd77848f651afbb2
IoriBranford <ioribranford@gmail.com>
2025-01-23 14:30:29 -0800

    l_func_3_0, l_func_1_vec2

--- include/engine/lua_func.h
+++ include/engine/lua_func.h
@@ -6,12 +6,15 @@
 #define l_func_0_0(f) int L_##f(lua_State *l) { f(); return 0; }
 #define l_func_1_0(f, at) int L_##f(lua_State *l) { f(luaL_check##at(l, 1)); return 0; }
 #define l_func_2_0(f, at, at2) int L_##f(lua_State *l) { f(luaL_check##at(l, 1), luaL_check##at2(l, 2)); return 0; }
+#define l_func_3_0(f, at, at2, at3) int L_##f(lua_State *l) { f(luaL_check##at(l, 1), luaL_check##at2(l, 2), luaL_check##at3(l, 3)); return 0; }
+#define l_func_4_0(f, at, at2, at3, at4) int L_##f(lua_State *l) { f(luaL_check##at(l, 1), luaL_check##at2(l, 2), luaL_check##at3(l, 3), luaL_check##at4(l, 4)); return 0; }
+
 #define l_func_0_1(f, rt) int L_##f(lua_State *l) { lua_push##rt(l, f()); return 1; }
 #define l_func_1_1(f, at, rt) int L_##f(lua_State *l) { lua_push##rt(l, f(luaL_check##at(l, 1))); return 1; }
 #define l_func_2_1(f, at, at2, rt) int L_##f(lua_State *l) { lua_push##rt(l, f(luaL_check##at(l, 1), luaL_check##at2(l, 2))); return 1; }
-#define l_func_4_0(f, at, at2, at3, at4) int L_##f(lua_State *l) { f(luaL_check##at(l, 1), luaL_check##at2(l, 2), luaL_check##at3(l, 3), luaL_check##at4(l, 4)); return 0; }
 
 #define l_func_0_vec2(f, vt) int L_##f(lua_State *l) { vt v = f(); lua_pushnumber(l, v.x); lua_pushnumber(l, v.y); return 2; }
+#define l_func_1_vec2(f, at, vt) int L_##f(lua_State *l) { vt v = f(luaL_check##at(l, 1)); lua_pushnumber(l, v.x); lua_pushnumber(l, v.y); return 2; }
 
 #define l_func_3_Color(f, at1, at2, at3) \
 int L_##f(lua_State *l) { \
