cd168eac3447fdbf6fc9a6b828fe64c7bcfddeac
IoriBranford <ioribranford@gmail.com>
2025-01-22 17:17:14 -0800

    Try out keyaxis and buttonaxis.

--- lua/gameplay/kitsune.lua
+++ lua/gameplay/kitsune.lua
@@ -1,26 +1,29 @@
 return function(obj, map)
     MapInputsToActions({
+        ["KAXIS_-LEFT+RIGHT"] = "movex",
+        ["KAXIS_-A+D"] = "movex",
+        ["P0_BAXIS_-LEFT_FACE_LEFT+LEFT_FACE_RIGHT"] = "movex",
+        P0_AXIS_LEFT_X = "movex",
         KEY_LEFT = "movex-",
         KEY_RIGHT = "movex+",
+        KEY_A = "movex-",
+        KEY_D = "movex+",
     })
     local sprite = obj:new_sprite(map)
     local tilename = "tile_stand0"
     sprite.tilenamedifnew = tilename
     while true do
         coroutine.yield()
-        local goingLeft, goingRight = IsActionDown("movex-"), IsActionDown("movex+")
-        if goingLeft and not goingRight then
+        local movex = GetActionPosition("movex")
+        if movex < 0 then
             tilename = "tile_walk2"
-            sprite.tilenamedifnew = tilename
-        elseif not goingLeft and goingRight then
+        elseif movex > 0 then
             tilename = "tile_walk0"
-            sprite.tilenamedifnew = tilename
         elseif tilename == "tile_walk0" then
             tilename = "tile_stand0"
-            sprite.tilenamedifnew = tilename
         elseif tilename == "tile_walk2" then
             tilename = "tile_stand2"
-            sprite.tilenamedifnew = tilename
         end
+        sprite.tilenamedifnew = tilename
     end
 end
