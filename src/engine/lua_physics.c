b883d0ce1e79e0e71c96040b92ecc0d8d8de6b6f
IoriBranford <ioribranford@gmail.com>
2025-01-28 12:43:21 -0800

    Bind cpArbiter class

--- src/engine/lua_physics.c
+++ src/engine/lua_physics.c
@@ -121,7 +121,7 @@ int L_cpBody_UpdateSprite(lua_State *l) {
 
 void L_cpBody_EachArbiter_iter(cpBody *body, cpArbiter *arbiter, lua_State *l) {
     lua_pushvalue(l, 2);
-    lua_pushlightuserdata(l, arbiter); // TODO make userdata class for Arbiter
+    class_newuserdata(l, cpArbiter, arbiter);
     if (lua_pcall(l, 1, 0, 0) != LUA_OK) lua_error(l);
 }
 
@@ -222,6 +222,44 @@ class_luaopen(cpShape,
     class_method_reg(cpShape, RemoveFromSpace)
 )
 
+class_index_and_newindex(cpArbiter)
+class_getterf_and_setterf(cpArbiter, *, number, Restitution, cpArbiterGetRestitution, cpArbiterSetRestitution)
+class_getterf_and_setterf(cpArbiter, *, number, Friction, cpArbiterGetFriction, cpArbiterSetFriction)
+class_getterf_and_setterf_vec2(cpArbiter, *, cpVect, SurfaceVelocity, cpArbiterGetSurfaceVelocity, cpArbiterSetSurfaceVelocity)
+
+/// "This function should only be called from a post-solve, post-step or cpBodyEachArbiter callback."
+// class_getterf_vec2(cpArbiter, *, cpVect, TotalImpulse, cpArbiterTotalImpulse)
+// class_getterf_vec2(cpArbiter, *, cpVect, KE, cpArbiterTotalKE)
+
+class_func_0_2ud(cpArbiter, *, GetShapes, cpArbiterGetShapes, cpShape, *, cpShape, *)
+class_func_0_2ud(cpArbiter, *, GetBodies, cpArbiterGetBodies, cpBody, *, cpBody, *)
+
+// TODO? ContactPoints
+
+class_getterf(cpArbiter, *, boolean, IsFirstContact, cpArbiterIsFirstContact)
+class_getterf(cpArbiter, *, boolean, IsRemoval, cpArbiterIsRemoval)
+class_getterf(cpArbiter, *, integer, NumContactPoints, cpArbiterGetCount)
+class_getterf_vec2(cpArbiter, *, cpVect, Normal, cpArbiterGetNormal)
+class_func_1_vec2(cpArbiter, *, GetPointA, cpArbiterGetPointA, integer, cpVect)
+class_func_1_vec2(cpArbiter, *, GetPointB, cpArbiterGetPointB, integer, cpVect)
+class_func_1_1(cpArbiter, *, GetDepth, cpArbiterGetDepth, integer, number)
+
+class_luaopen(cpArbiter,
+    class_method_reg(cpArbiter, __index),
+    class_method_reg(cpArbiter, __newindex),
+    class_getter_and_setter_reg(cpArbiter, Restitution),
+    class_getter_and_setter_reg(cpArbiter, Friction),
+    class_getter_and_setter_reg(cpArbiter, SurfaceVelocity),
+    class_method_reg(cpArbiter, GetShapes),
+    class_method_reg(cpArbiter, GetBodies),
+    class_getter_reg(cpArbiter, IsFirstContact),
+    class_getter_reg(cpArbiter, IsRemoval),
+    class_getter_reg(cpArbiter, NumContactPoints),
+    class_getter_reg(cpArbiter, Normal),
+    class_method_reg(cpArbiter, GetPointA),
+    class_method_reg(cpArbiter, GetPointB),
+    class_method_reg(cpArbiter, GetDepth))
+
 int luaopen_physics(lua_State *l) {
     luaL_Reg staticMethods[] = {
         class_method_reg(physics, body),
@@ -235,6 +273,7 @@ int luaopen_physics(lua_State *l) {
 
     lua_cpcall(l, luaopen_cpBody, NULL);
     lua_cpcall(l, luaopen_cpShape, NULL);
+    lua_cpcall(l, luaopen_cpArbiter, NULL);
 
     return 0;
 }
