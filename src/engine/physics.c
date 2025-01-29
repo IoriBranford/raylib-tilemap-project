6525bd58c08b2279d1f0e7f52677db370275309f
IoriBranford <ioribranford@gmail.com>
2025-01-28 16:08:08 -0800

    NewTMXObjectBody -> AddNewTMXObjectBody
    To imply that it creates the body and adds it to the space.

--- src/engine/physics.c
+++ src/engine/physics.c
@@ -119,6 +119,14 @@ cpBody* NewTMXObjectBody(tmx_object *obj, tmx_map *map) {
     cpBody *body = cpBodyNew(1, HUGE_VAL);
     cpBodySetPosition(body, cpv(obj->x, obj->y));
     cpBodySetAngle(body, obj->rotation * DEG2RAD);
+    GiveBodyTMXObjectShapes(body, obj, map->tiles, cpv(0, 0));
+    return body;
+}
+
+cpBody* AddNewTMXObjectBody(tmx_object *obj, tmx_map *map) {
+    cpBody *body = cpBodyNew(1, HUGE_VAL);
+    cpBodySetPosition(body, cpv(obj->x, obj->y));
+    cpBodySetAngle(body, obj->rotation * DEG2RAD);
     cpSpaceAddBody(space, body);
     GiveBodyTMXObjectShapes(body, obj, map->tiles, cpv(0, 0));
     return body;
